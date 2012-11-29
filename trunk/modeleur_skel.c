82702789aafae48b4b777d0037b905e7289737be
LucasSaurel <LucasSaurel@635fe907-f9a4-9c06-0f45-23c465ebae0f>
2012-11-29 15:02:48 +0000

    Squelette récupéré depuis le TP1 de FAIN

---
+++ trunk/modeleur_skel.c
@@ -0,0 +1,169 @@
+#include <stdio.h>
+#include "modeleurlib.h"
+
+void remplir(int xgraine, int ygraine)
+{
+    couleur c = val_point(xgraine, ygraine);
+    
+    if(c == BLANC || c == ROUGE ) return;
+    
+    if( xgraine < 0 || xgraine > hauteur_ecran() || ygraine < 0 || ygraine > largeur_ecran() )
+    {
+        return;
+    }
+
+    change_point(xgraine,ygraine,ROUGE);
+    remplir(xgraine-1, ygraine);
+    remplir(xgraine, ygraine-1);
+    remplir(xgraine+1, ygraine);
+    remplir(xgraine, ygraine+1);
+}
+
+
+void tracer_segment(int xA,int yA,int xB,int yB, int octant)
+{
+    printf("Segment dans l'octant %d rapporté au 1er : (%d,%d) ; (%d,%d)\n", octant, xA, yA, xB, yB);
+    int dx = xB - xA;
+    int dy = yB - yA;
+    int incr1 = 2*dy;
+    int incr2 = 2*(dy-dx);
+    int x = xA, y = yA;
+    int d = 2*dy-dx;
+
+    while(x < xB+1)
+    {
+//        printf("Boucle tracer_segment (xB==%d) : x==%d\n", xB, x);
+        switch (octant)
+        {
+            case 1 : change_point(x, y, BLANC); break;
+            case 2 : change_point(y, x, BLANC); break;
+            case 3 : change_point(y, -x, BLANC); break;
+            case 4 : change_point(-x, y, BLANC); break;
+            case 5 : change_point(-x, -y, BLANC); break;
+            case 6 : change_point(-y, -x, BLANC); break;
+            case 7 : change_point(-y, x, BLANC); break;
+            case 8 : change_point(x, -y, BLANC); break;
+        }
+        
+        x++;
+        if(d < 0)
+        {
+            d = d + incr1;
+        }
+        else
+        {
+            y++;
+            d = d + incr2;
+        }
+    }
+}
+
+
+void tracer_segment_vertical(int xA, int yA, int yB) {
+    if (yA<yB) {
+        for (int y = yA; y < yB+1; y++) {
+            change_point(xA, y, BLANC);
+        }
+    }
+    else {
+        for (int y = yA; y > yB-1; y--) {
+            change_point(xA, y, BLANC);
+        }
+    }
+}
+
+
+void tracer_segment_horizontal(int yA, int xA, int xB) {
+    if (xA<xB) {
+        for (int x = xA; x < xB+1; x++) {
+            change_point(x, yA, BLANC);
+        }
+    }
+    else {
+        for (int x = xA; x > xB-1; x--) {
+            change_point(x, yA, BLANC);
+        }
+    }
+}
+
+
+void segment(int xA,int yA,int xB,int yB)
+{
+    int dx = xB - xA;
+    int dy = yB - yA;
+    
+    printf("Points cliqués pour le segment : (%d, %d), (%d,%d)\n", xA, yA, xB, yB);
+
+    if (dx == 0)
+    {
+        tracer_segment_vertical(xA, yA, yB);
+    }
+    else if (dy == 0)
+    {
+        tracer_segment_horizontal(yA, xA, xB);
+    }
+    else if(dx > 0 && dy > 0 && dx > dy) // 1er octant
+    {
+        tracer_segment(xA, yA, xB, yB, 1);
+    }
+    else if(dx > 0 && dy > 0 && dx < dy) // 2eme octant
+    {
+        tracer_segment(yA, xA, yB, xB, 2);
+    }
+    else if(dx < 0 && dy > 0 && abs(dx) < dy) /* 3eme octant /!\ */
+    {
+        tracer_segment(-yB, xB, -yA, xA, 3);
+    }
+    else if(dx < 0 && dy > 0 && abs(dx) > dy) // 4eme octant
+    {
+        tracer_segment(-xA, yA, -xB, yB, 4);
+    }
+    else if(dx < 0 && dy < 0 && abs(dx) > abs(dy)) // 5eme octant
+    {
+        tracer_segment(-xA, -yA, -xB, -yB, 5);
+    }
+    else if(dx < 0 && dy < 0 && abs(dx) < abs(dy)) // 6eme octant
+    {
+        tracer_segment(-yA, -xA, -yB, -xB, 6);
+    }
+    else if(dx > 0 && dy < 0 && dx < abs(dy)) /* 7eme octant /!\ */
+    {
+        tracer_segment(yB, -xB, yA, -xA, 7);
+    }
+    else if(dx > 0 && dy < 0 && dx > abs(dy)) // 8eme octant
+    {
+        tracer_segment(xA, -yA, xB, -yB, 8);
+    }
+}
+
+
+void action_bouton_droit(int x, int y, int fin_click)
+{
+  if(fin_click) remplir(x,y);
+}
+
+
+void action_bouton_milieu(int x, int y, int fin_click)
+{
+  static int x0,y0;
+  if(fin_click) printf("fin"); else printf("debut");
+  printf(" appui bouton du milieu de la souris\n");
+  if(fin_click) {
+    segment(x0,y0,x,y);
+  }
+  else {
+    x0=x; y0=y;
+  }
+}
+
+
+void action_clavier(unsigned char touche, int x, int y)
+{
+  printf("touche %c appuyee\n", touche);
+}
+
+int main(int nargs, char **args)
+{
+  main_Modeleurlib(nargs,args);
+  return 0;
+}
---
+++ trunk/modeleurlib.c
@@ -0,0 +1,208 @@
+#include "modeleurlib.h"
+
+static int TAILLE_PIXEL=10;
+static int reshape_deja_lance=0;
+
+static couleur image[MAXX][MAXY];
+static int Width,Height;
+
+static struct{	unsigned char r,g,b;} couleurs[]=
+{
+  {  0,  0,  0},/* NOIR    */
+  {255,255,255},/* BLANC   */
+  {255,  0,  0},/* ROUGE   */
+  {  0,255,  0},/* VERT    */
+  {  0,  0,255},/* BLEU    */
+  {255,255,  0},/* JAUNE   */
+  {255,  0,255},/* MAGENTA */
+  {  0,255,255},/* CYAN    */
+  {128,128,128},/* GRIS    */
+};
+
+
+//-----------------------------------------------------------------
+// procedure lancee quand on retaille la fenetre
+
+static void reshape(int width, int height)
+{
+  reshape_deja_lance=1;
+  Width=width;
+  Height=height;
+  printf("reshape(%d,%d)\n",width,height);
+  glViewport(0, 0, width, height);
+  glLoadIdentity();
+  gluOrtho2D(-0.5,((double)width)/TAILLE_PIXEL-0.5,
+			 -0.5,((double)height)/TAILLE_PIXEL-0.5);
+}
+
+
+//-----------------------------------------------------------------
+// procedure permettant de changer la taille des pixels pour mieux
+// voir comment fonctionnent les algorithmes de geometrie discrete.
+
+void change_taille_pixel(int t)
+{
+	TAILLE_PIXEL=t;
+	if(reshape_deja_lance)
+	{
+		glPointSize(TAILLE_PIXEL);
+		reshape(Width,Height);
+		glutPostRedisplay();
+		// Indicate that things will have to be redisplayed
+	}
+}
+
+
+//-----------------------------------------------------------------
+// procedure lancee pour afficher l'image. En l'occurrence, elle
+// affiche le contenu du tableau image[][].
+
+static couleur current_color=-1;
+
+static void display(void)
+{
+	printf("display\n");
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	int x,y,w,h;
+	if(Width<MAXX) w=Width; else w=MAXX;
+	if(Height<MAXY) h=Height; else h=MAXY;
+
+	glBegin(GL_POINTS);
+	for(x=0;x<w;x++)
+		for(y=0;y<h;y++)
+		{
+			couleur c=image[x][y];
+			if(c)
+			{
+				if(c!=current_color)
+				{
+					glColor3ub(couleurs[c].r,couleurs[c].g,couleurs[c].b);
+					current_color=c;
+				}
+				glVertex2i(x,y);
+			}
+		}
+	glEnd();
+	glFinish();
+	glFlush();
+	glutReportErrors();
+}
+
+
+//-----------------------------------------------------------------
+// Change la couleur du point de coordonnées (x,y)
+
+void change_point(int x, int y, couleur c)
+{
+	if(x<0 || y<0 || x>=MAXX || y>=MAXY) return;
+
+	image[x][y]=c;
+
+	if(c!=current_color)
+	{
+		glColor3ub(couleurs[c].r,couleurs[c].g,couleurs[c].b);
+		current_color=c;
+	}
+	glBegin(GL_POINTS);
+	glVertex2i(x,y);
+	glEnd();
+	glFlush();
+}
+
+
+//-----------------------------------------------------------------
+// C'est la pipette : retourne la couleur du point de coord (x,y).
+
+couleur val_point(int x, int y)
+{
+  if(x<0 || y<0 || x>=MAXX || y>=MAXY) return NOIR;
+  return image[x][y];
+}
+
+
+//-----------------------------------------------------------------
+// L'action à réaliser lorsqu'on clique :
+// Pour le bouton gauche : si la couleur préexistant est le noir,
+// alors on la change en blanc, sinon, on la remet à noir.
+// Pour le bouton du milieu et le bouton droit, on execute
+// action_bouton_milieu et action_bouton_droit (à definir).
+
+static void souris(int b,int s,int x,int y)
+{
+	int x2=x/TAILLE_PIXEL;
+	int y2=(Height-1-y)/TAILLE_PIXEL;
+	switch(b)
+	{
+	case GLUT_LEFT_BUTTON:
+	{
+		if(s==GLUT_DOWN)
+			change_point(x2,y2,!val_point(x2,y2));
+		break;
+	}
+    case GLUT_MIDDLE_BUTTON:
+	{
+		action_bouton_milieu(x2,y2,s==GLUT_UP);
+		break;
+	}
+	case GLUT_RIGHT_BUTTON:	action_bouton_droit(x2,y2,s==GLUT_UP);    break;
+  }
+  /*glutPostRedisplay();*/
+	glutReportErrors();
+}
+
+
+
+//-----------------------------------------------------------------
+// Les initialisations de glut
+
+int main_Modeleurlib(int nargs, char **args)
+{
+	glutInit(&nargs,args);
+	glutInitDisplayMode(GLUT_RGB|GLUT_SINGLE);
+	glutCreateWindow(args[0]);
+
+    /* Gestion des evenements */
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutMouseFunc(souris);
+	glutKeyboardFunc(action_clavier);
+
+	glPointSize(TAILLE_PIXEL);
+	glutMainLoop();
+	return 0;
+}
+
+//-----------------------------------------------------------------
+// Retourne la largeur de l'écran (en nombre de gros pixels)
+
+int largeur_ecran(void)
+{
+	int w=(Width-1)/TAILLE_PIXEL+1;
+
+	if(w<=MAXX)	return w;
+	else		return MAXX;
+}
+
+//-----------------------------------------------------------------
+// Retourne la largeur de l'écran (en nombre de gros pixels)
+
+int hauteur_ecran(void)
+{
+	int h=(Height-1)/TAILLE_PIXEL+1;
+	if(h<=MAXY)	return h;
+	else 		return MAXY;
+}
+
+//-----------------------------------------------------------------
+// Fonction qui change la couleur de tous les pixels à NOIR.
+
+void efface_tout(void)
+{
+	int x;
+	for(x=0;x<MAXX;x++)
+		memset(&image[x][0],0,MAXY*sizeof(couleur));
+
+	glClear(GL_COLOR_BUFFER_BIT); /* au cas ou on fait des calculs apres */
+	glutPostRedisplay();
+}
