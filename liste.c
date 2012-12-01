6c828a8690847ee6a5736aeca538c77243ce184e
LucasSaurel <LucasSaurel@635fe907-f9a4-9c06-0f45-23c465ebae0f>
2012-12-01 11:27:07 +0000

    Gestion des listes de points

--- liste.c
+++ liste.c
@@ -3,71 +3,87 @@
 #include "liste.h"
 
 
-Liste liste_ajout (Liste l, Point nvelt)
+Liste liste_creer (Point p)
 {
-	Liste p;
-	p = malloc(sizeof(struct maillon));
-	p->point = nvelt;
-	p->suivant = l;
-	return(p);
+    Liste l;
+	l.point = p;
+	return l;
+}
+
+
+Liste liste_ajout (Liste l, Point p)
+{
+	Liste liste;
+	liste.point = p;
+	liste.suivant = &l;
+	return liste;
 }
 
 
 Liste liste_insere(Liste l, Point e)
 {
-	Liste res = l;
-	if (l == NULL) {
-		res = liste_ajout(NULL, e);
+	Liste tmp = l;
+	
+    if (tmp.suivant == NULL) {
+		tmp.suivant = malloc(sizeof(struct maillon));
+        tmp.suivant->point = e;
 	}
 	else {
-		l->suivant = liste_insere(l->suivant, e);
+		tmp = *tmp.suivant;
+        liste_insere(tmp, e);
 	}
-	return(res);
+	return l;
 }
 
 
-int liste_appartient(Point x, Liste l)
+int liste_appartient(Point p, Liste l)
 {
-	if (l == NULL) return(0);
-	if (point_egaux(x, l->point)) return(1);
-	return(liste_appartient(x, l->suivant));
+	int res = 0;
+    
+    if (point_egaux(p, l.point)) res = 1;
+	else if (l.suivant != NULL) res = liste_appartient(p, *l.suivant);
+    return res;
 }
 
 
-void liste_concat(Liste *l1, Liste l2)
+int liste_taille(Liste l)
 {
-	Liste l = *l1;
-	if (l == NULL) *l1 = l2;
-	else {
-		while (l->suivant != NULL) {
-			l = l->suivant;
-		}
-		l->suivant = l2;
-	}
+    int res = 0;
+    
+    while (l.suivant != NULL) {
+        l = *l.suivant;
+        res += 1;
+    }
+    return res;
+}
+
+
+Liste liste_concat(Liste l1, Liste l2)
+{
+	Liste l = l1;
+
+    while (l.suivant != NULL) {
+        l = *l.suivant;
+    }
+    
+    l.suivant = &l2;
+    return l1;
 }
 
 
 int liste_position(Point e, Liste l)
 {
-	int res = 0;
-	if (l != NULL)
-    {
-		if (point_egaux(e, l->point)) res = 1;
-		else res = 1 + liste_position(e,l->suivant);
-	}
-	return(res);
-}
+	int res = 1;
+    
+    while (!point_egaux(e, l.point)) {
+        if (l.suivant != NULL) {
+            res += 1;
+            l = *l.suivant;
+        }
+        else return 0;
+    }
 
-
-int liste_incluse(Liste l1, Liste l2)
-{
-	int res=1;
-	if (l1 != NULL)
-    {
-		if (liste_position(l1->point, l2) == 0) res=0;
-		else res = liste_incluse(l1->suivant, l2);
-	}
-	return(res);
+	return res;
 }
 
 
